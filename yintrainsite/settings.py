fb32711b7b8bc4a2fb903e4e7a4715e1342afa4c
kishimi8 <kishimi8@gmail.com>
2016-10-10 21:54:51 +0100

    added templates

--- yintrainsite/settings.py
+++ yintrainsite/settings.py
@@ -70,20 +70,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yintrainsite.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': 'yininfo',
-        'USER': 'postgres',
-        'PASSWORD': 'kishimi8I',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': "django.db.backends.postgresql",
+#         'NAME': 'yininfo',
+#         'USER': 'postgres',
+#         'PASSWORD': 'kishimi8I',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
